d85e8414d745965d3bb272c90f7d87a04172855f
Tadas Talaikis <tadas@entiercapital.com>
2024-10-29 15:39:48 +0200

    chore: export lib + main

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,4 @@
+export declare function sha256(a: string): Promise<string>;
+export declare function generateKeyPair(length: number, algo: string): Promise<string>;
+export declare function signData(data: string, privateKey: string): Promise<string>;
+//# sourceMappingURL=index.d.ts.map
