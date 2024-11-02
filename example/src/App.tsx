8c3c5b3cb5413bed5ba971fd65e7b780ce437b5d
Tadas Talaikis <tadas@entiercapital.com>
2024-11-02 20:24:25 +0200

    chore: downgrade rn to 0.75

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,14 +5,19 @@ export default function App() {
   useEffect(() => {
     const serve = async () => {
       const result = await sha256("string");
+      console.info('result', result)
       if (!result) throw new Error('Failed sha256')
       const keys = await generateKeyPair(2048, 'RSA');
+      console.info('keys', keys)
       if (!keys) throw new Error('Failed generateKeyPair')
       const signed = await signData('string', keys.privateKey);
+      console.info('signed', signed)
       if (!signed) throw new Error('Failed signData')
       const aesKey = await generateAESKey(256)
       const encrypted = await aesEncrypt('test', aesKey)
+      console.info('encrypted', encrypted)
       const decrypted = await aesDecrypt(encrypted, aesKey)
+      console.info('decrypted', decrypted)
       if (decrypted !== 'test') throw new Error('Failed AES')
     }
 
